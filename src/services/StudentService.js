ec1c5768abcc8b95a2902c75f2dd45261e0481b7
RAGHU SIR <javabyraghu@gmail.com>
2022-11-29 17:32:43 +0530

    First Commit

---
+++ src/services/StudentService.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:9690/v1/api/student';
+
+class StudentService {
+
+      getAllStudents() {
+            return axios.get(`${baseUrl}/all`);
+      }
+
+      createStudent(student) {
+            return axios.post(`${baseUrl}/create`, student);
+      }
+
+      deleteStudent(id) {
+            return axios.delete(`${baseUrl}/remove/${id}`, id);
+      }
+
+      getOneStudent(id) {
+            return axios.get(`${baseUrl}/find/${id}`, id);
+      }
+
+      updateStudent(student) {
+            return axios.post(`${baseUrl}/modify`, student);
+      }
+}
+
+export default new StudentService();
